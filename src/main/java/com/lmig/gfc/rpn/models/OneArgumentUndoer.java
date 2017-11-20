345c282021921f5f12d15c1f5c094513a204155f
Doug George <douglas.george@libertymutual.com>
2017-11-20 16:57:33 -0500

    A basic RPN calculator.

---
+++ src/main/java/com/lmig/gfc/rpn/models/OneArgumentUndoer.java
@@ -0,0 +1,23 @@
+package com.lmig.gfc.rpn.models;
+
+import java.util.Stack;
+
+public class OneArgumentUndoer {
+
+	private double first;
+
+	public OneArgumentUndoer(double first) {
+		this.first = first;
+
+	}
+
+	public void undo(Stack<Double> stack) {
+		stack.pop();
+		parentUndo(stack);
+	}
+	
+	protected void parentUndo(Stack<Double> stack) {
+		stack.push(first);
+	}
+
+}
